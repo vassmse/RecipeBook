9ba0e5363b6f6674a65881e75614117bdcc89e8b
Emese Vass <vassmse@gmail.com>
2017-09-21 18:57:24 +0200

    hamburgermenu

--- RecipeBook/MainPage.xaml.cs
+++ RecipeBook/MainPage.xaml.cs
@@ -28,9 +28,42 @@ namespace RecipeBook
         {
             this.InitializeComponent();
             RecipeBook = new MainViewModel();
+
+            hamburgerMenuControl.ItemsSource = MenuItem.GetMainItems();
+            hamburgerMenuControl.OptionsItemsSource = MenuItem.GetOptionsItems();
+        }
+
+        private void OnMenuItemClick(object sender, ItemClickEventArgs e)
+        {
+            var menuItem = e.ClickedItem as MenuItem;
+            contentFrame.Navigate(menuItem.PageType);
         }
 
         public MainViewModel RecipeBook { get; set; }
 
+    }
+
+
+    public class MenuItem
+    {
+        public Symbol Icon { get; set; }
+        public string Name { get; set; }
+        public Type PageType { get; set; }
+
+        public static List<MenuItem> GetMainItems()
+        {
+            var items = new List<MenuItem>();
+            items.Add(new MenuItem() { Icon = Symbol.Accept, Name = "MenuItem1", PageType = null });
+            items.Add(new MenuItem() { Icon = Symbol.Send, Name = "MenuItem2", PageType = null });
+            items.Add(new MenuItem() { Icon = Symbol.Shop, Name = "MenuItem3", PageType = null });
+            return items;
+        }
+
+        public static List<MenuItem> GetOptionsItems()
+        {
+            var items = new List<MenuItem>();
+            items.Add(new MenuItem() { Icon = Symbol.Setting, Name = "OptionItem1", PageType = null });
+            return items;
+        }
     }
 }
