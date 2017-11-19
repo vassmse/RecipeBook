4496e55bad4b6eef950a79b929ef07797bc27326
Emese Vass <vassmse@gmail.com>
2017-11-19 15:33:43 +0100

    ..

--- RecipeBookBLL/EntryPoints/RecipeManager.cs
+++ RecipeBookBLL/EntryPoints/RecipeManager.cs
@@ -17,93 +17,43 @@ namespace RecipeBookBLL.Models
     {
         public RecipeManager()
         {
-            
+
         }
 
         public List<Recipe> GetRecipes()
         {
-            var recipes = new List<Recipe>();
-
-            using (SqliteConnection db = new SqliteConnection("Filename=recipeBook.db"))
+            try
             {
-                db.Open();
+                var recipes = new List<Recipe>();
 
-                SqliteCommand selectRecipesCommand = new SqliteCommand("SELECT Recipe.id, recipe.name, recipe.description, recipe.PREPARATION, recipetype.id, recipetype.name from Recipe, RecipeType where Recipe.RECIPETYPE_ID = RecipeType.ID", db);
-
-                SqliteDataReader query = selectRecipesCommand.ExecuteReader();
-
-                while (query.Read())
+                using (SqliteConnection db = new SqliteConnection("Filename=recipeBook.db"))
                 {
-                    //Get ingredients
-                    var ingredients = new List<Ingredient>();
-                    SqliteCommand selectIngredientsCommand = new SqliteCommand("SELECT ingredients.id, ingredients.quantity, ingredients.unit,RawMaterial.id, RawMaterial.name  from Ingredients, RawMaterial where Ingredients.RECIPE_ID==@id AND ingredients.RAWMATERIAL_ID == rawmaterial.id; ", db);
-                    selectIngredientsCommand.Parameters.AddWithValue("@id", query.GetInt32(0));
-                    SqliteDataReader inQuery = selectIngredientsCommand.ExecuteReader();
+                    db.Open();
 
-                    while(inQuery.Read())
+                    SqliteCommand selectRecipesCommand = new SqliteCommand("SELECT Recipe.id, recipe.name, recipe.description, recipe.PREPARATION, recipetype.id, recipetype.name from Recipe, RecipeType where Recipe.RECIPETYPE_ID = RecipeType.ID", db);
+
+                    SqliteDataReader query = selectRecipesCommand.ExecuteReader();
+
+                    while (query.Read())
                     {
-                        var material = new RawMaterial { Id = inQuery.GetInt32(3), Name = inQuery.GetString(4) };
-                        ingredients.Add(new Ingredient { Id = inQuery.GetInt32(0), Quantity = inQuery.GetInt32(1), Unit = inQuery.GetString(2), Material = material });
+                        var ingredients = GetIngredients(query.GetInt32(0));
+                        var type = new RecipeType { Id = query.GetInt32(4), Name = query.GetString(5) };
+
+                        recipes.Add(new Recipe { Id = query.GetInt32(0), Name = query.GetString(1), Description = query.GetString(2), PreparationTime = query.GetInt32(3), Type = type, Ingredients = ingredients });
                     }
 
-                    var type = new RecipeType { Id = query.GetInt32(4), Name = query.GetString(5) };
-                    recipes.Add(new Recipe { Id = query.GetInt32(0), Name = query.GetString(1), Description = query.GetString(2), PreparationTime = query.GetInt32(3), Type = type, Ingredients = ingredients });
+                    db.Close();
                 }
 
-                db.Close();
+                return recipes;
             }
-
-            return recipes;
-
-
-            //var rawFlour = new RawMaterial { Id = 1, Name = "liszt" };
-            //var rawWater = new RawMaterial { Id = 2, Name = "víz" };
-            //var rawSalt = new RawMaterial { Id = 3, Name = "só" };
-            //var rawPea = new RawMaterial { Id = 4, Name = "borsó" };
-
-            //var flour = new Ingredient { Id = 1, Material = rawFlour, Quantity = 5, Unit = "kg" };
-            //var water = new Ingredient { Id = 2, Material = rawWater, Quantity = 2, Unit = "liter" };
-            //var salt = new Ingredient { Id = 3, Material = rawSalt, Quantity = 2, Unit = "csipet" };
-            //var pea = new Ingredient { Id = 4, Material = rawSalt, Quantity = 1, Unit = "kg" };
-
-            //var lasagnaIngredients = new List<Ingredient>
-            //{
-            //    flour,
-            //    water,
-            //    salt
-            //};
-
-            //var peaSoupIngredients = new List<Ingredient>
-            //{
-            //    flour,
-            //    water,
-            //    salt,
-            //    pea
-            //};
-
-            //var pizzaIngredients = new List<Ingredient>
-            //{
-            //    flour,
-            //    water
-            //};
-
-
-            //var soup = new RecipeType { Id = 1, Name = "Leves" };
-            //var mainCourse = new RecipeType { Id = 2, Name = "Főétel" };
-
-            //AddRecipe(new Recipe { Id = 1, Name = "Borsóleves", Description = "Meg kell főzni a borsólevest. Nagyon finom.", PreparationTime = 30, Ingredients = peaSoupIngredients, Type = soup });
-
-            //return new List<Recipe>()
-            //    {
-            //        new Recipe {Id=1, Name="Borsóleves", Description="Meg kell főzni a borsólevest. Nagyon finom.", PreparationTime=30, Ingredients=peaSoupIngredients, Type=soup},
-            //        new Recipe {Id=2, Name="Hagymaleves", Description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis hendrerit nulla, vel molestie libero. In nec ultricies magna, ultricies molestie ipsum. Mauris non dignissim velit. Etiam malesuada blandit mauris eu maximus. Quisque ornare, felis nec scelerisque mollis, risus dolor posuere magna, in gravida quam mi id nisi. Nullam mattis consequat ex. Cras nulla neque, dictum ac urna et, vestibulum feugiat ex. Pellentesque malesuada accumsan ligula, vel fringilla lacus facilisis sit amet. Proin convallis tempor arcu, ac placerat libero pretium ut. Praesent hendrerit nisl at lobortis viverra. Fusce vitae velit odio. Nam ut tortor sed purus finibus sollicitudin quis at ante. Ut sodales dolor vel eros mollis suscipit. Donec eu nulla id urna ultricies consequat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae", PreparationTime=20, Ingredients=lasagnaIngredients, Type=soup},
-            //        new Recipe {Id=3, Name="Lasagna", Description="Meg kell sütni. Minnél több, annál jobb.", PreparationTime=140, Ingredients=lasagnaIngredients, Type=mainCourse},
-            //        new Recipe {Id=4, Name="Pizza" , Description="Ezt is meg kell sütni", PreparationTime=180, Ingredients=pizzaIngredients, Type=mainCourse},
-            //        new Recipe {Id=5, Name="Rakottkrumpli" , Description="Meg kell sütni", PreparationTime=200, Ingredients=peaSoupIngredients, Type=mainCourse}
-            //    };
+            catch (Exception e)
+            {
+                //TODO
+                Console.WriteLine(e);
+                return new List<Recipe>();
+            }
         }
-
-
 
         public List<RecipeType> GetRecipeTypes()
         {
@@ -167,13 +117,12 @@ namespace RecipeBookBLL.Models
 
         public void AddRecipe(Recipe recipe)
         {
-            using (SqliteConnection db = new SqliteConnection("Filename=recipeBook.db"))
+            try
             {
-                db.Open();
-                
-                
-                try
+                using (SqliteConnection db = new SqliteConnection("Filename=recipeBook.db"))
                 {
+                    db.Open();
+
                     //Insert recipe
                     SqliteCommand insertRecipe = new SqliteCommand();
                     insertRecipe.Connection = db;
@@ -205,31 +154,32 @@ namespace RecipeBookBLL.Models
                         insertIngredients.Connection = db;
 
                         insertIngredients.CommandText = "INSERT INTO Ingredients VALUES (NULL, @RawMat, @Quantity, @Unit, @RecipeId);";
-                        insertIngredients.Parameters.AddWithValue("@RawMat", ingredient?.Material?.Id??0);
-                        insertIngredients.Parameters.AddWithValue("@Quantity", ingredient?.Quantity??0);
-                        insertIngredients.Parameters.AddWithValue("@Unit", ingredient?.Unit??"");
+                        insertIngredients.Parameters.AddWithValue("@RawMat", ingredient?.Material?.Id ?? 0);
+                        insertIngredients.Parameters.AddWithValue("@Quantity", ingredient?.Quantity ?? 0);
+                        insertIngredients.Parameters.AddWithValue("@Unit", ingredient?.Unit ?? "");
                         insertIngredients.Parameters.AddWithValue("@RecipeId", recipeId);
 
                         insertIngredients.ExecuteReader();
                     }
 
-                    GetRecipes();
+                    //SqliteCommand delete1 = new SqliteCommand();
+                    //delete1.Connection = db;
+                    //delete1.CommandText = "DELETE FROM Recipe;";
+                    //var tmp = delete1.ExecuteScalar();
 
-                    SqliteCommand delete1 = new SqliteCommand();
-                    delete1.Connection = db;
-                    delete1.CommandText = "DELETE FROM Recipe;";
-                    var tmp = delete1.ExecuteScalar();
+                    //SqliteCommand delete2 = new SqliteCommand();
+                    //delete2.Connection = db;
+                    //delete2.CommandText = "DELETE FROM Ingredients;";
+                    //var tmp2 = delete2.ExecuteScalar();
 
-                    SqliteCommand delete2 = new SqliteCommand();
-                    delete2.Connection = db;
-                    delete2.CommandText = "DELETE FROM Ingredients;";
-                    var tmp2 = delete2.ExecuteScalar();
+                    db.Close();
                 }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                }
-                db.Close();
+            }
+            catch (Exception e)
+            {
+                //TODO
+                Console.WriteLine(e);
+                throw;
             }
 
         }
@@ -245,6 +195,29 @@ namespace RecipeBookBLL.Models
         }
 
         #region Private DB methods
+
+        private List<Ingredient> GetIngredients(int recipeId)
+        {
+            var ingredients = new List<Ingredient>();
+
+            using (SqliteConnection db = new SqliteConnection("Filename=recipeBook.db"))
+            {
+                db.Open();
+                SqliteCommand selectCommand = new SqliteCommand("SELECT ingredients.id, ingredients.quantity, ingredients.unit,RawMaterial.id, RawMaterial.name  from Ingredients, RawMaterial where Ingredients.RECIPE_ID==@id AND ingredients.RAWMATERIAL_ID == rawmaterial.id; ", db);
+                selectCommand.Parameters.AddWithValue("@id", recipeId);
+
+                SqliteDataReader query = selectCommand.ExecuteReader();
+
+                while (query.Read())
+                {
+                    var material = new RawMaterial { Id = query.GetInt32(3), Name = query.GetString(4) };
+                    ingredients.Add(new Ingredient { Id = query.GetInt32(0), Quantity = query.GetInt32(1), Unit = query.GetString(2), Material = material });
+                }
+                db.Close();
+            }
+
+            return ingredients;
+        }
 
         private void CreateTables()
         {
